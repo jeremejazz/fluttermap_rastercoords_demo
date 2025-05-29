2f789caf01397feb48188c6136b273cae49c511e
Jereme <jeremejazz15@gmail.com>
2025-05-29 11:35:47 +0800

    add basic map

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 void main() {
   runApp(const RasterCoordsDemo());
 }
+
 class RasterCoordsDemo extends StatelessWidget {
   const RasterCoordsDemo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('Flutter Map Raster Coords Example'),),
-        body: Container(
-          child: const Text("Hello World!"),
+        appBar: AppBar(title: const Text('Map View')),
+        body: FlutterMap(
+          options: MapOptions(
+            initialZoom: 5,
+            initialCenter: LatLng(11.3352855, 124.3544939),
+          ),
+          children: [
+            TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            ),
+          ],
         ),
-      )
+      ),
     );
   }
 }
-
