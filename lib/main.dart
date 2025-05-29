ef0392bce0244bc4420b7c352c7b27447d849aca
Jereme <jeremejazz15@gmail.com>
2025-05-29 14:03:49 +0800

    add switch for localhost url

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_rastercoords/flutter_map_rastercoords.dart';
@@ -7,6 +10,25 @@ void main() {
 }
 
 class RasterCoordsDemo extends StatelessWidget {
+  final String _defaultMapUrl =
+      "http://localhost:8080/map_tiles/{z}/{x}/{y}.png";
+  final String _androidMapUrl =
+      "http://10.0.2.2:8080/map_tiles/{z}/{x}/{y}.png";
+
+  /// To ensure compatibility for running in localhost
+  /// on other platforms. You won't be needing this
+  /// if your map is already running on a remote server.
+  String getMapUrl() {
+    if (kIsWeb) {
+      return _defaultMapUrl;
+    }
+    if (Platform.isAndroid) {
+      return _androidMapUrl;
+    }
+
+    return _defaultMapUrl;
+  }
+
   final rc = RasterCoords(width: 6143, height: 4600);
   RasterCoordsDemo({super.key});
 
@@ -32,9 +54,7 @@ class RasterCoordsDemo extends StatelessWidget {
             ),
           ),
           children: [
-            TileLayer(
-              urlTemplate: 'http://10.0.2.2:8080/map_tiles/{z}/{x}/{y}.png',
-            ),
+            TileLayer(urlTemplate: getMapUrl()),
             MarkerLayer(
               markers: [
                 Marker(
